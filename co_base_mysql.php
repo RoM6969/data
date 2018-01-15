32e5e8587e00dc52a827a088926d0767dc7f4314
Tim <32301460+tim007221@users.noreply.github.com>
2018-01-15 09:32:13 +0100

    avec requette sql

--- co_base_mysql.php
+++ co_base_mysql.php
@@ -3,15 +3,79 @@
 
 try
 {
-    $bdd = new PDO('mysql:host=;dbname=guerreti;charset=utf8', 'root', '');
-}
-catch(Exception $e)
-{
-    die('Erreur : '.$e->getMessage());
+    $bdd = new PDO('mysql:host=127.0.0.1;dbname=guerreti;charset=utf8', 'root','123');
 	
+	echo "toto";
+	//on recupere tout le contenut de la table bdpollution2_5
+	
+	$reponse_2_5 = $bdd->query("SELECT * FROM `bdpollution2_5` WHERE `id` = 11 ");
+	
+	
+	$donnees = $reponse->fetch();
+	
+	
+	
+	//on recupere tout le contenut de la table bdpollution10
+	
+	$reponse = $bdd->query("SELECT * FROM `bdpollution2_5` WHERE `id_10` = 11 ");
+	
+	
+	$donnees = $reponse->fetch();
+	
+	
+	
+	//on recupere tout le contenut de la table bdpollution2_5
+	
+	$reponse = $bdd->query("SELECT * FROM `bdpollution2_5` WHERE `id` = 11 ");
+	
+	
+	$donnees = $reponse->fetch();
+	
+	
+	
+	//on recupere tout le contenut de la table bdpollution2_5
+	
+	$reponse = $bdd->query("SELECT * FROM `bdpollution2_5` WHERE `id` = 11 ");
+	
+	
+	$donnees = $reponse->fetch();
+	
+	
+	
+	//on recupere tout le contenut de la table bdpollution2_5
+	
+	$reponse = $bdd->query("SELECT * FROM `bdpollution2_5` WHERE `id` = 11 ");
+	
+	
+	$donnees = $reponse->fetch();
+	
+	
+	
+	//on recupere tout le contenut de la table bdpollution2_5
+	
+	$reponse = $bdd->query("SELECT * FROM `bdpollution2_5` WHERE `id` = 11 ");
+	
+	
+	$donnees = $reponse->fetch();
+	
+	
+	//on recupere tout le contenut de la table bdpollution2_5
+	
+	$reponse = $bdd->query("SELECT * FROM `bdpollution2_5` WHERE `id` = 11 ");
+	
+	
+	$donnees = $reponse->fetch();
+
+	
+	
+	
+	$resul_valeur_particule10 = $valeur_particule10->fetch();
+	
+
 }
-
-
-
-//$reponce = mysql_query("SELECT * FORM bdppllution10");
+catch(Exception $e) {
+    echo $e->getMessage();
+}
 ?>
+<?php echo $donnees['2002']; ?> euros !<br />
+
